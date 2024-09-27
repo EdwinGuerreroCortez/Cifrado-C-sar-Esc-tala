880d7ddb2576e110e7a1499d21d7ce2c5eeb9383
EdwinGuerreroCortez <20221030@uthh.edu.mx>
2024-09-27 10:26:23 -0600

    add: cesar

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@
 import React from 'react';
 import EncryptionMethods from './Components/EncryptionMethods';
 import './App.css';
-import './dark-mode.css'; // Estilos para el modo oscuro
- 
+
 function App() {
   return (
         <EncryptionMethods /> 
